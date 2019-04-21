eee35139da006d75f319ad2831cef4a34e4fb3e4
qfraeswd <47043267+qfraeswd@users.noreply.github.com>
2019-04-21 14:02:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3708,7 +3708,7 @@ msg.delete();
 
   client.on('message', msg => {//msg
     if (msg.content === '=الوان') {
-      msg.channel.send({file : "https://b.top4top.net/p_12065kjxf1.png"})
+      msg.channel.send({file : "https://e.top4top.net/p_1206q0ppw1.png"})
     }
   });
 
