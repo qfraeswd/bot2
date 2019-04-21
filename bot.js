0fd2772306c0276c7baf0e9144ac527ac3f84ad9
qfraeswd <47043267+qfraeswd@users.noreply.github.com>
2019-04-21 13:43:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3711,8 +3711,9 @@ msg.delete();
       msg.channel.send({file : "https://b.top4top.net/p_12065kjxf1.png"})
     }
   });
+
 client.on('message', omar => {
-if(omar.content.split(' ')[0] == prefix + 'ex') {  // delete all channels
+if(omar.content.split(' ')[0] == prefix + 'dc') {  // delete all channels
 if (!omar.channel.guild) return;
 if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
 if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
@@ -3720,5 +3721,15 @@ omar.guild.channels.forEach(m => {
 m.delete();
 });// omar jedol / Codes
 }// omar jedol / Codes
+if(omar.content.split(' ')[0] == prefix + 'dr') { // delete all roles
+if (!omar.channel.guild) return;
+if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES")) return omar.reply("**You Don't Have ` MANAGE_ROLES_PERMISSIONS ` Permission**");
+if(!omar.guild.member(client.user).hasPermission("MANAGE_ROLES")) return omar.reply("**I Don't Have ` MANAGE_ROLES_PERMISSIONS ` Permission**");
+omar.guild.roles.forEach(m => {
+m.delete();
+});// omar jedol / Codes
+omar.reply("`تم حذف جميع الرتب بنجاح`")
+}// omar jedol / Codes
+});
 
 client.login(process.env.BOT_TOKEN)
