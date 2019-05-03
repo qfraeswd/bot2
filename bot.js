6e30b8ec20bb80098f1d64f4aaf4d14b4ad66b2d
qfraeswd <47043267+qfraeswd@users.noreply.github.com>
2019-05-03 23:11:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3745,4 +3745,10 @@ color: 'RANDOM'})
 }
 });
 
+  client.on('message',  => {
+    if (content === '.') {
+      reply('** #``-`` __W__elcome __T__ø __V__ictoria ** :champagne_glass: ');
+    }
+  });
+
 client.login(process.env.BOT_TOKEN)
